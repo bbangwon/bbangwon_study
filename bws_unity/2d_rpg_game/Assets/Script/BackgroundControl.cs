da1735e5419cc6e658ad7a12a9fed41c46dafe15
bbangwon <bbangwon.chung@gmail.com>
2016-02-11 17:58:22 +0900

    4장 2D 아처 캐릭터 제작까지

---
+++ bws_unity/2d_rpg_game/Assets/Script/BackgroundControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundControl : MonoBehaviour {
+    //Background와 Foreground의 Animator를 등록시킬 변수
+    //public으로 선언된 변수는 인스펙터 뷰(Inspector View)에서 접근과 수정이 가능합니다.
+    public Animator[] mBackgrounds;
+
+	// Use this for initialization
+	void Start () {
+        FlowControl(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void FlowControl(float speed)
+    {
+        //등록된 모든 애니메이터들의 speed를 조정
+        foreach (Animator bg in mBackgrounds)
+            bg.speed = speed;
+    }
+}
