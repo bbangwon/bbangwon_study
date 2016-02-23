32a315576b6ed55d64d662591f54f8f5cef3a675
bbangwon <bbangwon.chung@gmail.com>
2016-02-24 02:12:17 +0900

    7.1장 완료

---
+++ bws_unity/2d_rpg_game/Assets/Script/LayerSort.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class LayerSort : MonoBehaviour {
+    public enum Layers
+    {
+        Background,
+        Foreground,
+        Effect,
+        UI
+    }
+    //레이어 이름
+    public Layers mLayerName;
+    //레이어 오더
+    public int mOrderNumber;
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = mLayerName.ToString();
+        GetComponent<Renderer>().sortingOrder = mOrderNumber;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
