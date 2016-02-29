135079aab28ed71dd0ff55a5193fe1426f445244
bbangwon <bbangwon.chung@gmail.com>
2016-02-29 19:57:55 +0900

    unity 5.3.3 update, UNET study add

---
+++ bws_unity/UNET_study1/Assets/script/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+	void Start () {
+        if (isLocalPlayer)
+            name = "Player Me";
+        else
+            name = "Player " + GetComponent<NetworkIdentity>().netId.Value.ToString();
+	}
+}
