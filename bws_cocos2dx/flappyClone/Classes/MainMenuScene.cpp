2c56a1f287cab4423390a6475fba254992508bd1
bbangwon <bbangwon.chung@gmail.com>
2016-06-02 00:16:53 +0900

    MainMenuScene 구현

--- bws_cocos2dx/flappyClone/Classes/MainMenuScene.cpp
+++ bws_cocos2dx/flappyClone/Classes/MainMenuScene.cpp
@@ -1,4 +1,6 @@
 #include "MainMenuScene.h"
+#include "GameScene.h"
+#include "Definitions.h"
 
 USING_NS_CC;
 
@@ -30,7 +32,33 @@ bool MainMenuScene::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	auto backgroundSprite = Sprite::create("background.png");
+	backgroundSprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+
+	this->addChild(backgroundSprite);
+
+	auto titleSprite = Sprite::create("Title.png");
+	titleSprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height - titleSprite->getContentSize().height));
+
+	this->addChild(titleSprite);
+
+
+	auto playItem = MenuItemImage::create("Play Button.png", "Play Button Clicked.png", CC_CALLBACK_1(MainMenuScene::GoToGameScene, this));
+	playItem->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+
+	auto menu = Menu::create(playItem, nullptr);
+	menu->setPosition(Point::ZERO);
+
+	this->addChild(menu);
+
     return true;
+}
+
+void MainMenuScene::GoToGameScene(cocos2d::Ref * sender)
+{
+	auto scene = GameScene::createScene();
+
+	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
 }
 
 
